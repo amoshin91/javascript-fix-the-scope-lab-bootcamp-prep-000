0c96ebd189eb6723472f2d1a9751bcca95281350
Yongwoo Shin <amoshinihsoma@gmail.com>
2018-11-12 19:12:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,4 +31,7 @@ var funkyFunction = function outsideFunk() {
 // NOTE: you only need to modify the code below this line.
 
 var theFunk = function funkyFunction() {
+  return function insideFunk(){
+    
+  }
 }
